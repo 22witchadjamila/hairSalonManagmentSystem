7faf037444d2d7326995ba95d0602d55a4fb6453
22witchadjamila <djamilawitcha9@gmail.com>
2026-03-24 14:06:59 +0200

    feature: Create the ServiceFactory.java class

---
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/factory/ServiceFactory.java
@@ -0,0 +1,32 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Service;
+
+public class ServiceFactory {
+
+        public static Service createService (String Id, String name,double price, int duration) throws IllegalArgumentException {
+
+            if (Id == null || Id.isEmpty())
+                throw new IllegalArgumentException("Service ID is required");
+
+            if (name == null || name.isEmpty())
+                throw new IllegalArgumentException("Service name is required");
+
+            if (price <= 0)
+                throw new IllegalArgumentException("Price must be greater than 0");
+
+            if (duration <= 0)
+                throw new IllegalArgumentException("Duration must be greater than 0");
+
+
+            return new Service.Builder()
+
+                    .setServiceId(Id)
+                    .setServiceName(name)
+                    .setPrice(price)
+                    .setDuration(duration)
+                    .build();
+
+        }
+    }
+
