98501f1f110fa5f4b06f9d111b7b9711f9faf2d7
22witchadjamila <djamilawitcha9@gmail.com>
2026-03-26 10:14:55 +0200

    feature: Created the ServiceRepository CLASS AND ITS INTERFACE

---
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/repository/service/IServiceRepository.java
@@ -0,0 +1,6 @@
+package za.ac.cput.repository.service;
+import za.ac.cput.domain.Service;
+import za.ac.cput.repository.IRepository;
+public interface IServiceRepository extends IRepository<Service, String> {
+}
+
---
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/repository/service/impl/ServiceRepository.java
@@ -0,0 +1,63 @@
+package za.ac.cput.repository.service.impl;
+import za.ac.cput.domain.Service;
+import za.ac.cput.repository.service.IServiceRepository;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ServiceRepository  implements IServiceRepository  {
+
+    private static ServiceRepository repository = null;
+    private Set<Service> serviceDB;
+
+    private ServiceRepository() {
+        serviceDB = new HashSet<>();
+    }
+
+    public static ServiceRepository getRepository() {
+        if (repository == null) {
+            repository = new ServiceRepository();
+        }
+        return repository;
+    }
+
+    @Override
+    public Service create(Service service) {
+        serviceDB.add(service);
+        return service;
+    }
+
+    @Override
+    public Service read(String id) {
+        return serviceDB.stream()
+                .filter(s -> s.getServiceId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public Service update(Service service) {
+        Service oldService = read(service.getServiceId());
+        if (oldService != null) {
+            serviceDB.remove(oldService);
+            serviceDB.add(service);
+            return service;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        Service service = read(id);
+        if (service != null) {
+            serviceDB.remove(service);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Set<Service> getAll() {
+        return serviceDB;
+    }
+
+}
