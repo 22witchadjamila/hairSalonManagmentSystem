460820c2214d6da4725513384e7dfa7baa414051
Reece <Reece.H17.Joseph@gmail.com>
2026-03-26 14:22:44 +0200

    Completed Payment domain and PaymentFactory.

---
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/factory/PaymentFactory.java
@@ -0,0 +1,30 @@
+/* PaymentFactory.java
+   Factory class for creating Payment domain objects
+   Author: Reece Josephs [218152701]
+   Date: [23 March 2025]
+*/
+
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Payment;
+
+public class PaymentFactory {
+
+    public static Payment createPayment(String paymentId, double amount, String paymentMethod) {
+
+        if (paymentId == null || paymentId.isEmpty())
+            return null;
+
+        if (paymentMethod == null || paymentMethod.isEmpty())
+            return null;
+
+        if (amount <= 0)
+            return null;
+
+        return new Payment.Builder()
+                .setPaymentId(paymentId)
+                .setAmount(amount)
+                .setPaymentMethod(paymentMethod)
+                .build();
+    }
+}
