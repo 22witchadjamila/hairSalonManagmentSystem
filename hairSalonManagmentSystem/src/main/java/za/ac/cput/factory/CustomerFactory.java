bab5c5db45e4454dff8c77f16319ef6b73b07683
22witchadjamila <djamilawitcha9@gmail.com>
2026-03-26 19:45:23 +0200

    feature: i just add the dependencies to be able to test

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/factory/CustomerFactory.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/factory/CustomerFactory.java
@@ -23,7 +23,7 @@ public class CustomerFactory {
                 .setCustomerId(id)
                 .setName(name)
                 .setEmail(email)
-                .setPhoneNumber(phoneNumber)
+                .setPhoneNumber(Integer.parseInt(phoneNumber))
                 .build();
     }
 }
