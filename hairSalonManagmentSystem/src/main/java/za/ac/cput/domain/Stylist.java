595138891bf6dcca2a223da34b5c15161ae0955a
MarcCput <230701876@mycput.ac.za>
2026-07-29 13:33:50 +0200

    Updating Stylist entity class

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/domain/Stylist.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/domain/Stylist.java
@@ -5,67 +5,121 @@ Student Number: 240160711
 Date: 20 March 2026
 */
 
-
 package za.ac.cput.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "stylists")
 public class Stylist {
 
-    private int stylistId;
-    private String name;
-    private String speciality;
-    private int experienceYears;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String stylistId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String specialty;
+    private boolean isActive;
 
-    private Stylist(Builder builder) {
-        this.stylistId = builder.stylistId;
-        this.name = builder.name;
-        this.speciality = builder.speciality;
-        this.experienceYears = builder.experienceYears;
+    @ManyToOne
+    @JoinColumn(name = "appointment_appointment_id")
+    private Appointment appointment;
+
+    public Appointment getAppointment() {
+        return appointment;
     }
 
-    public int getStylistId() {
+
+    public Stylist() {}
+
+    public Stylist(Builder builder) {
+        this.stylistId = builder.stylistId;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.email = builder.email;
+        this.phoneNumber = builder.phoneNumber;
+        this.specialty = builder.specialty;
+        this.isActive = builder.isActive;
+    }
+
+    public String getStylistId() {
         return stylistId;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
     public String getSpeciality() {
-        return speciality;
+        return specialty;
     }
 
-    public int getExperienceYears() {
-        return experienceYears;
+    public boolean isActive() {
+        return isActive;
     }
 
-    public static class Builder {
-        private int stylistId;
-        private String name;
-        private String speciality;
-        private int experienceYears;
+    public static class Builder{
+        private String stylistId;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String phoneNumber;
+        private String specialty;
+        private boolean isActive;
 
-        public Builder setStylistId(int stylistId) {
+        public Builder setStylistId(String stylistId) {
             this.stylistId = stylistId;
             return this;
         }
 
-        public Builder setName(String name) {
-            this.name = name;
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
             return this;
         }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
 
         public Builder setSpeciality(String speciality) {
-            this.speciality = speciality;
+            this.specialty = speciality;
             return this;
         }
 
-        public Builder setExperienceYears(int experienceYears) {
-            this.experienceYears = experienceYears;
+        public Builder setActive(boolean active) {
+            this.isActive = active;
             return this;
         }
 
         public Stylist build() {
-            return new Stylist(this);
+            return new Stylist();
         }
+
     }
 }
