22b11c8ae5386d576412ee2a671f40e686753b71
MarcCput <230701876@mycput.ac.za>
2026-07-29 14:48:37 +0200

    Updating NotificationFactory

---
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/factory/NotificationFactory.java
@@ -0,0 +1,53 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Appointment;
+import za.ac.cput.domain.Notification;
+import za.ac.cput.domain.enums.NotificationChannel;
+import za.ac.cput.domain.enums.NotificationStatus;
+import za.ac.cput.domain.enums.NotificationType;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.UUID;
+
+public class NotificationFactory {
+
+    public static Notification buildAppointmentReminder(Appointment appointment,
+                                                        NotificationChannel channel){
+        if(appointment == null || channel == null) return null;
+
+        String message = "Reminder: You have an appointment on "
+                + appointment.getAppointmentDate()
+                + " at " + appointment.getStartTime()
+                + ". Please arrive 5 minutes early.";
+
+        LocalDateTime scheduleAt = LocalDateTime.of(
+                appointment.getAppointmentDate().minusDays(1),
+                LocalTime.of(9, 0));
+
+        return new Notification.Builder()
+                .setNotificationId(UUID.randomUUID().toString())
+                .setMessage(message)
+                .setType(NotificationType.APPOINTMENT_REMINDER)
+                .setChannel(channel)
+                .setStatus(NotificationStatus.SCHEDULED)
+                .setScheduledAt(scheduleAt)
+                .setAppointment(appointment)
+                .build();
+    }
+
+    public static Notification buildPromotionNotification(String message,
+                                                          NotificationChannel channel){
+        if (message == null || message.isBlank()) return null;
+        if (channel == null) return null;
+
+        return new Notification.Builder()
+                .setNotificationId(UUID.randomUUID().toString())
+                .setMessage(message)
+                .setType(NotificationType.PROMOTION)
+                .setChannel(channel)
+                .setStatus(NotificationStatus.SCHEDULED)
+                .setScheduledAt(LocalDateTime.now())
+                .build();
+    }
+}
