d367bcecf2c846a694e67ec5599ce7acba0c1a08
MarcCput <230701876@mycput.ac.za>
2026-07-29 14:30:58 +0200

    Refactoring the FeedbackFactory

---
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/factory/FeedbackFactory.java
@@ -0,0 +1,30 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Appointment;
+import za.ac.cput.domain.Customer;
+import za.ac.cput.domain.Feedback;
+import za.ac.cput.domain.enums.AppointmentStatus;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class FeedbackFactory {
+
+    public static Feedback buildFeedback(Appointment appointment,
+                                         Customer customer,
+                                         int rating, String comment) {
+        if(appointment == null || customer == null) return null;
+        if(appointment.getStatus() != AppointmentStatus.COMPLETED) return null;
+        if(rating < 1 || rating > 5) return null;
+
+        return new Feedback.Builder()
+                .setReviewId(UUID.randomUUID().toString())
+                .setAppointment(appointment)
+                .setCustomer(customer)
+                .setRating(rating)
+                .setComment(comment)
+                .setSubmittedAt(LocalDateTime.now())
+                .setVerified(false)
+                .build();
+    }
+}
