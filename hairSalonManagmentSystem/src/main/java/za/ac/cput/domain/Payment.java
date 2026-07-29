937a24d1cee4f4ec45a2de63205f827fc4c97a83
MarcCput <230701876@mycput.ac.za>
2026-07-29 12:18:10 +0200

    Updating payment entity class

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/domain/Payment.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/domain/Payment.java
@@ -6,62 +6,151 @@
 
 package za.ac.cput.domain;
 
+import jakarta.persistence.*;
+import za.ac.cput.domain.enums.PaymentMethod;
+import za.ac.cput.domain.enums.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="payments")
 public class Payment {
+
+    @Id
     private String paymentId;
-    private double amount;
-    private String paymentMethod;
 
-    private Payment() {}
+    private BigDecimal amount;
+    private BigDecimal discount;
+    private BigDecimal finalAmount;
 
-    private Payment(Builder builder) {
-        this.paymentId = builder.paymentId;
-        this.amount = builder.amount;
-        this.paymentMethod = builder.paymentMethod;
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod method;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    private LocalDateTime paidAt;
+    private String transactionRef;
+
+    @ManyToOne
+    @JoinColumn(name = "appointment_id")
+    private Appointment appointment;
+
+    @ManyToOne
+    @JoinColumn(name = "promotion_id")
+    private Promotion promotion;
+
+    protected Payment(){}
+
+    public Payment(Builder builder){
+        this.paymentId      = builder.paymentId;
+        this.amount         = builder.amount;
+        this.discount       = builder.discount;
+        this.finalAmount    = builder.finalAmount;
+        this.method         = builder.method;
+        this.status         = builder.status;
+        this.paidAt         = builder.paidAt;
+        this.transactionRef = builder.transactionRef;
+        this.appointment    = builder.appointment;
+        this.promotion      = builder.promotion;
     }
 
     public String getPaymentId() {
         return paymentId;
     }
 
-    public double getAmount() {
+    public BigDecimal getAmount() {
         return amount;
     }
 
-    public String getPaymentMethod() {
-        return paymentMethod;
+    public BigDecimal getDiscount() {
+        return discount;
     }
 
-    @Override
-    public String toString() {
-        return "Payment{" +
-                "paymentId='" + paymentId + '\'' +
-                ", amount=" + amount +
-                ", paymentMethod='" + paymentMethod + '\'' +
-                '}';
+    public BigDecimal getFinalAmount() {
+        return finalAmount;
     }
 
+    public LocalDateTime getPaidAt() {
+        return paidAt;
+    }
 
-    public static class Builder {
+    public String getTransactionRef() {
+        return transactionRef;
+    }
+
+    public PaymentMethod getMethod() {return method;}
+
+    public PaymentStatus getStatus() {return status;}
+
+    public Appointment getAppointment() {return appointment;}
+
+    public Promotion getPromotion() {return promotion;}
+
+    public static class Builder{
         private String paymentId;
-        private double amount;
-        private String paymentMethod;
+        private BigDecimal amount;
+        private BigDecimal discount;
+        private BigDecimal finalAmount;
+        private PaymentMethod method;
+        private PaymentStatus status;
+        private LocalDateTime paidAt;
+        private String transactionRef;
+        private Appointment appointment;
+        private Promotion promotion;
 
         public Builder setPaymentId(String paymentId) {
             this.paymentId = paymentId;
             return this;
         }
 
-        public Builder setAmount(double amount) {
+        public Builder setAmount(BigDecimal amount) {
             this.amount = amount;
             return this;
         }
 
-        public Builder setPaymentMethod(String paymentMethod) {
-            this.paymentMethod = paymentMethod;
+        public Builder setDiscount(BigDecimal discount) {
+            this.discount = discount;
             return this;
         }
 
-        public Payment build() {
+        public Builder setFinalAmount(BigDecimal finalAmount) {
+            this.finalAmount = finalAmount;
+            return this;
+        }
+
+        public Builder setPaidAt(LocalDateTime paidAt) {
+            this.paidAt = paidAt;
+            return this;
+        }
+
+        public Builder setTransactionRef(String transactionRef) {
+            this.transactionRef = transactionRef;
+            return this;
+        }
+
+        public Builder setMethod(PaymentMethod method) {
+            this.method = method;
+            return this;
+        }
+
+        public Builder setStatus(PaymentStatus status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder setAppointment(Appointment appointment) {
+            this.appointment = appointment;
+            return this;
+        }
+
+        public Builder setPromotion(Promotion promotion) {
+            this.promotion = promotion;
+            return this;
+        }
+
+        public Payment build(){
             return new Payment(this);
         }
     }
