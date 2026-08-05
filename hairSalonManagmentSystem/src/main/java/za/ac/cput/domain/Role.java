d73cb4bb586b11e320c72e45ae50c2e5f9b178d4
MarcCput <230701876@mycput.ac.za>
2026-08-06 00:44:34 +0200

    Updating the Role entity class

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/domain/Role.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/domain/Role.java
@@ -9,4 +9,48 @@ public class Role {
     private String name;
     private String description;
 
+    protected Role() {}
+
+    public Role(Builder builder) {
+        this.roleId = builder.roleId;
+        this.name = builder.name;
+        this.description = builder.description;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static class Builder{
+        private String roleId;
+        private String name;
+        private String description;
+
+        public Builder setRoleId(String roleId) {
+            this.roleId = roleId;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Role build(){
+            return new Role(this);
+        }
+    }
 }
