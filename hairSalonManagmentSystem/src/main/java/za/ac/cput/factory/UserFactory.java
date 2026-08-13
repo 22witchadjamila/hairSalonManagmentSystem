fd90eb7d1316a4eecfe179164d7a235b0ea8abd9
MarcCput <230701876@mycput.ac.za>
2026-08-13 12:53:20 +0200

    Adding UserFactory class

---
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/factory/UserFactory.java
@@ -0,0 +1,30 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Role;
+import za.ac.cput.domain.User;
+import za.ac.cput.domain.valueobject.Email;
+import za.ac.cput.util.Helper;
+
+import java.time.LocalDateTime;
+
+public class UserFactory {
+
+    public static User buildUser(String username, String email,
+                                 String firstName, String lastName, Role role){
+        if(Helper.isNullOrEmpty(username)) return null;
+        if(!Helper.isValidEmail(email)) return null;
+        if(Helper.isNullOrEmpty(firstName)) return null;
+        if(Helper.isNullOrEmpty(lastName)) return null;
+
+        return new User.Builder()
+                .setUserId(Helper.generateId())
+                .setUsername(username.trim())
+                .setEmail(Email.of(email))
+                .setFirstName(firstName.trim())
+                .setLastName(lastName.trim())
+                .setActive(true)
+                .setCreatedAt(LocalDateTime.now())
+                .setRole(role)
+                .build();
+    }
+}
