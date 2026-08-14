24101bf87cb21badfc72af2a9e9ecd6686b6b1e7
MarcCput <230701876@mycput.ac.za>
2026-08-14 12:24:49 +0200

    Updating ProductInventoryRepository

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/repository/ProductInventoryRepository.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/repository/ProductInventoryRepository.java
@@ -1,4 +1,7 @@
 package za.ac.cput.repository;
 
-public interface ProductInventoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.ProductInventory;
+
+public interface ProductInventoryRepository extends JpaRepository<ProductInventory, String> {
 }
