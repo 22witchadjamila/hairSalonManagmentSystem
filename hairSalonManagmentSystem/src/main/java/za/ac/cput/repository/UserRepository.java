20c14d09752c6d9a39a437d8cc9443bcf1a30542
MarcCput <230701876@mycput.ac.za>
2026-08-14 12:51:44 +0200

    Updating UserRepository

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/repository/UserRepository.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package za.ac.cput.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
 }
