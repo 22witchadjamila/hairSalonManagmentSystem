ec4a674bfae522ae112665a578ed37e6039841fb
MarcCput <230701876@mycput.ac.za>
2026-08-14 12:49:49 +0200

    Updating StylistRepository

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/repository/StylistRepository.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/repository/StylistRepository.java
@@ -1,4 +1,10 @@
 package za.ac.cput.repository;
 
-public interface StylistRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.Stylist;
+
+import java.util.List;
+
+public interface StylistRepository extends JpaRepository<Stylist, String> {
+    List<Stylist> findByIsActiveTrue();
 }
