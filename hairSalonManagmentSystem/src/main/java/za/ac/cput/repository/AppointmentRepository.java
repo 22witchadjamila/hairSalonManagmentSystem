8aa5371c1b41d634c8191d6d5ecc29c4b2ede80e
MarcCput <230701876@mycput.ac.za>
2026-08-14 09:10:55 +0200

    Addding repositories and Updating AppointmentRepository

---
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/repository/AppointmentRepository.java
@@ -0,0 +1,13 @@
+package za.ac.cput.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.Appointment;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface AppointmentRepository extends JpaRepository<Appointment,String> {
+    List<Appointment> findByStylist_StylistIdAndAppointmentDate(String stylistId, LocalDate
+            appointmentDate);
+    List<Appointment> findByCustomer_CustomerId(String customerId);
+}
