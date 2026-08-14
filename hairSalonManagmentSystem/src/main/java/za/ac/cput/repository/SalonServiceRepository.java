e0e26b778af4d3ebe305541067a96ec8a6442963
MarcCput <230701876@mycput.ac.za>
2026-08-14 12:45:42 +0200

    Updating SalonServiceRepository

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/repository/SalonServiceRepository.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/repository/SalonServiceRepository.java
@@ -1,4 +1,12 @@
 package za.ac.cput.repository;
 
-public interface SalonServiceRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.SalonService;
+import za.ac.cput.domain.enums.ServiceCategory;
+
+import java.util.List;
+
+public interface SalonServiceRepository extends JpaRepository<SalonService, String> {
+    List<SalonService> findByCategory(ServiceCategory category);
+    List<SalonService> findByIsActiveTrue();
 }
