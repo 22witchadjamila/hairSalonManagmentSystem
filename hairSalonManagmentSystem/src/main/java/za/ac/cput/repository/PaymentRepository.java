904a0ce145a762633157090cd355db126c48aff4
MarcCput <230701876@mycput.ac.za>
2026-08-14 12:21:29 +0200

    Updating PaymentRepository

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/repository/PaymentRepository.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/repository/PaymentRepository.java
@@ -1,4 +1,10 @@
 package za.ac.cput.repository;
 
-public interface PaymentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.Payment;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, String> {
+    Optional<Payment> findByAppointment_AppointmentId(String appointmentId);
 }
