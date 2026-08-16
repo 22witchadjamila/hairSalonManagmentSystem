7f3834303417d9eb7e6a33f1dad396f93c31579d
MarcCput <230701876@mycput.ac.za>
2026-08-17 00:44:16 +0200

    Updating IService interface so it has Generic CRUD contract every service implements

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/service/IService.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/service/IService.java
@@ -1,4 +1,13 @@
 package za.ac.cput.service;
 
-public interface IService {
+import java.util.List;
+
+/** Generic CRUD contract every service implements,
+  *  so callers deal with one consistent shape. */
+public interface IService <T, ID>{
+    T create(T entity);
+    T read(ID id);
+    T update(T entity);
+    void delete(ID id);
+    List<T> getAll();
 }
