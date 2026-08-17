01578fb970726d27cc267b9be7b1dd5f602e6318
MarcCput <230701876@mycput.ac.za>
2026-08-17 10:07:41 +0200

    Updating FeedbackService interface

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/service/IFeedbackService.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/service/IFeedbackService.java
@@ -1,4 +1,8 @@
 package za.ac.cput.service;
 
-public interface IFeedbackService {
+import za.ac.cput.domain.Feedback;
+
+public interface IFeedbackService extends IService<Feedback, String> {
+    Feedback submitFeedback(String appointmentId, String customerId,
+                            int rating, String comment);
 }
