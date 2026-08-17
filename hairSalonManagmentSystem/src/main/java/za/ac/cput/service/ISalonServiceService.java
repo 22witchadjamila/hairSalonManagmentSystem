e057f0f04229b9f6b6fdf48f9c9c1c60b3ad5843
MarcCput <230701876@mycput.ac.za>
2026-08-17 15:13:44 +0200

    Updating ISalonServiceService interface

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/service/ISalonServiceService.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/service/ISalonServiceService.java
@@ -1,4 +1,15 @@
 package za.ac.cput.service;
 
-public interface ISalonServiceService {
+import za.ac.cput.domain.SalonService;
+import za.ac.cput.domain.enums.ServiceCategory;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface ISalonServiceService extends IService<SalonService,String> {
+    SalonService register(String name, String description, int durationMinutes,
+                          BigDecimal price, ServiceCategory category);
+
+    List<SalonService> findByCategory(ServiceCategory category);
+    List<SalonService> findActiveServices();
 }
