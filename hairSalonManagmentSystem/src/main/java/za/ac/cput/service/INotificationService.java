f4cc9536f025f36ca00b94659c7739395c56d96c
MarcCput <230701876@mycput.ac.za>
2026-08-17 14:19:41 +0200

    Updating

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/service/INotificationService.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/service/INotificationService.java
@@ -1,4 +1,9 @@
 package za.ac.cput.service;
 
-public interface INotificationService {
+import za.ac.cput.domain.Notification;
+import za.ac.cput.domain.enums.NotificationChannel;
+
+public interface INotificationService extends IService<Notification, String>{
+    Notification scheduleAppointmentReminder(String appointmentId, NotificationChannel channel);
+    Notification sendPromotionNotification(String message, NotificationChannel channel);
 }
--- hairSalonManagmentSystem/src/main/java/za/ac/cput/service/IPaymentService.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/service/IPaymentService.java
@@ -1,4 +1,14 @@
 package za.ac.cput.service;
 
-public interface IPaymentService {
+import za.ac.cput.domain.Payment;
+import za.ac.cput.domain.enums.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public interface IPaymentService extends IService<Payment, String>{
+    /** Processes a payment for an appointment, optionally applying
+     * a promo code.*/
+    Payment processPayment(String appointmentId, BigDecimal amount, PaymentMethod method,
+                           String promoCode);
+    Payment findByAppointment(String appointmentId);
 }
