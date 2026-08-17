0cbb7c07e18dd79fb3f049ed7a3014cfc54c4ea5
MarcCput <230701876@mycput.ac.za>
2026-08-17 14:47:39 +0200

    Updating IProductInventoryService interface

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/service/IProductInventoryService.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/service/IProductInventoryService.java
@@ -1,4 +1,20 @@
 package za.ac.cput.service;
 
-public interface IProductInventoryService {
+import za.ac.cput.domain.ProductInventory;
+import za.ac.cput.domain.SalonService;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface IProductInventoryService extends IService<ProductInventory, String>{
+    ProductInventory register(String name, String brand, String category, int stockQuantity,
+                              int reorderLevel, BigDecimal costPrice,
+                              BigDecimal sellingPrice, SalonService salonService);
+
+    /** Adjusts stock by delta (positive to restock to consume); rejects if it
+     * would go negative. */
+    ProductInventory adjustStock(String productId, int delta);
+
+    /** Products at or below their reorder level. */
+    List<ProductInventory> lowStock();
 }
