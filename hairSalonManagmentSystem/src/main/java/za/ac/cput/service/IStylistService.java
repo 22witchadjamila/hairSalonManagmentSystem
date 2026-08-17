9d6b7470a3396a9dd41974cba1eb9cd2ab2a4582
MarcCput <230701876@mycput.ac.za>
2026-08-17 15:21:44 +0200

    Updating IStylistService interface

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/service/IStylistService.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/service/IStylistService.java
@@ -1,4 +1,11 @@
 package za.ac.cput.service;
 
-public interface IStylistService {
+import za.ac.cput.domain.Stylist;
+
+import java.util.List;
+
+public interface IStylistService extends  IService<Stylist, String> {
+    Stylist register(String firstName, String lastName, String email,
+                     String phoneNumber, String speciality);
+    List<Stylist> findActiveStylists();
 }
