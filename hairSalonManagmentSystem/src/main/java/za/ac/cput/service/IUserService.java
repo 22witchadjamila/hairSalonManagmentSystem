c7bc83b08763c35363e5703a5357569757857997
MarcCput <230701876@mycput.ac.za>
2026-08-17 23:15:15 +0200

    Updating IUserService interface

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/service/IUserService.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/service/IUserService.java
@@ -1,4 +1,10 @@
 package za.ac.cput.service;
 
-public interface IUserService {
+import za.ac.cput.domain.Role;
+import za.ac.cput.domain.User;
+
+public interface IUserService extends IService<User, String>{
+    User register(String username, String email, String firstName,
+                  String lastName, Role role);
+    User findByUsername(String username);
 }
