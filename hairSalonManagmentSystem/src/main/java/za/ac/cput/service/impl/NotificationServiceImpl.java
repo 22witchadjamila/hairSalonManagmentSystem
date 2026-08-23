35ecdc8abd552f11c73956c65fc83a890c77c1d3
MarcCput <230701876@mycput.ac.za>
2026-08-24 00:02:00 +0200

    Updating NotificationServiceImpl

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/service/impl/NotificationServiceImpl.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/service/impl/NotificationServiceImpl.java
@@ -1,10 +1,16 @@
 package za.ac.cput.service.impl;
 
+import za.ac.cput.domain.Appointment;
 import za.ac.cput.domain.Notification;
+import za.ac.cput.domain.enums.NotificationChannel;
+import za.ac.cput.exception.InvalidOperationException;
 import za.ac.cput.exception.ResourceNotFoundException;
+import za.ac.cput.factory.NotificationFactory;
 import za.ac.cput.repository.AppointmentRepository;
 import za.ac.cput.repository.NotificationRepository;
 import za.ac.cput.service.INotificationService;
+
+import java.util.List;
 
 public class NotificationServiceImpl implements INotificationService {
 
@@ -37,4 +43,31 @@ public class NotificationServiceImpl implements INotificationService {
     public void delete(String id){
         repository.deleteById(id);
     }
+
+    @Override
+    public List<Notification> getAll(){
+        return repository.findAll();
+    }
+
+    @Override
+    public Notification scheduleAppointmentReminder(String appointmentId, NotificationChannel channel) {
+        Appointment appointment = appointmentRepository.findById(appointmentId)
+                .orElseThrow(() -> ResourceNotFoundException.forEntity("Appointment", appointmentId));
+
+        Notification notification = NotificationFactory.buildAppointmentReminder(appointment, channel);
+        if (notification == null) {
+            throw new InvalidOperationException("Could not schedule reminder for this appointment.");
+        }
+        return create(notification);
+    }
+
+    @Override
+    public Notification sendPromotionNotification(String message, NotificationChannel channel) {
+        Notification notification = NotificationFactory.buildPromotionNotification(message, channel);
+        if (notification == null) {
+            throw new InvalidOperationException("Invalid notification details provided.");
+        }
+        return create(notification);
+    }
+
 }
