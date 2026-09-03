0ed12bd2b7507e690a1f07b768c42bcc820ee0c7
Witcha Francisco <222894822@mycput.ac.za>
2026-09-03 13:30:39 +0200

    Add:Add APPOINTMENT_CONFIRMED:

--- hairSalonManagmentSystem/src/main/java/za/ac/cput/domain/enums/NotificationType.java
+++ hairSalonManagmentSystem/src/main/java/za/ac/cput/domain/enums/NotificationType.java
@@ -1,6 +1,6 @@
 package za.ac.cput.domain.enums;
 
 public enum NotificationType {
-    APPOINTMENT_REMINDER, PROMOTION,
+    APPOINTMENT_REMINDER, APPOINTMENT_CONFIRMED, PROMOTION,
     LOYALTY_UPDATE, GENERAL
 }
